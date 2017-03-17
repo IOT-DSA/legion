a27a58131611baaee4874881405a09bd81286011
Kenneth Endfinger <kaendfinger@gmail.com>
2017-03-16 21:54:09 -0400

    Disable Plugins Entirely

--- lib/src/tool/plugin_loader.dart
+++ lib/src/tool/plugin_loader.dart
@@ -4,41 +4,41 @@ import "dart:async";
 import "dart:io";
 
 //import "file://C:/tmp/__legion.plugins.dart" deferred as _WindowsPlugins;
-import "file:///tmp/__legion.plugins.dart" deferred as _UnixPlugins;
-
-const String _legionUnixPluginsPath = "/tmp/__legion.plugins.dart";
+//import "file:///tmp/__legion.plugins.dart" deferred as _UnixPlugins;
+//
+//const String _legionUnixPluginsPath = "/tmp/__legion.plugins.dart";
 //const String _legionWindowsPluginsPath = "C:\\tmp\__legion.plugins.dart";
 
 Future writePluginStub(String content) async {
-  var file = new File(
+//  var file = new File(
 //    Platform.isWindows ?
 //    _legionWindowsPluginsPath :
-    _legionUnixPluginsPath
-  );
-
-  if (!(await file.parent.exists())) {
-    await file.parent.create(recursive: true);
-  }
-
-  await file.writeAsString(content.trimLeft());
+//    _legionUnixPluginsPath
+//  );
+//
+//  if (!(await file.parent.exists())) {
+//    await file.parent.create(recursive: true);
+//  }
+//
+//  await file.writeAsString(content.trimLeft());
 }
 
 Future attemptToLoadPluginStub() async {
-  var file = new File(
+//  var file = new File(
 //    Platform.isWindows ?
 //    _legionWindowsPluginsPath :
-    _legionUnixPluginsPath
-  );
-
-  if (!(await file.exists())) {
-    return;
-  }
-
-  if (Platform.isWindows) {
+//    _legionUnixPluginsPath
+//  );
+//
+//  if (!(await file.exists())) {
+//    return;
+//  }
+//
+//  if (Platform.isWindows) {
 //    await _WindowsPlugins.loadLibrary();
 //    await _WindowsPlugins.init();
-  } else {
-    await _UnixPlugins.loadLibrary();
-    await _UnixPlugins.init();
-  }
+//  } else {
+//    await _UnixPlugins.loadLibrary();
+//    await _UnixPlugins.init();
+//  }
 }
